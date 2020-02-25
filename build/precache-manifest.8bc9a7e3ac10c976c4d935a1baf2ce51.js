0a9d85d298cadc1405e6679d02cd8c55efd99002
nbesh761 <55159411+nbesh761@users.noreply.github.com>
2020-02-25 12:53:51 -0700

    first react site

---
+++ build/precache-manifest.8bc9a7e3ac10c976c4d935a1baf2ce51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ae3984825df1331bce59d90431e54d",
+    "url": "/nbesh761/movie-app/index.html"
+  },
+  {
+    "revision": "90f237d1a0951fb6f171",
+    "url": "/nbesh761/movie-app/static/css/main.a34cbb79.chunk.css"
+  },
+  {
+    "revision": "eb3522a841ef625a11ce",
+    "url": "/nbesh761/movie-app/static/js/2.4c093274.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/nbesh761/movie-app/static/js/2.4c093274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f237d1a0951fb6f171",
+    "url": "/nbesh761/movie-app/static/js/main.5c91bb30.chunk.js"
+  },
+  {
+    "revision": "54a3c0ce690a0a4f8c49",
+    "url": "/nbesh761/movie-app/static/js/runtime-main.53c948d8.js"
+  },
+  {
+    "revision": "50fdac4f6b8f1ff081c1594353a60d7f",
+    "url": "/nbesh761/movie-app/static/media/cinema.50fdac4f.jpg"
+  },
+  {
+    "revision": "d38f8a7d68d3a1f7903fd432a8bce935",
+    "url": "/nbesh761/movie-app/static/media/logo.d38f8a7d.png"
+  }
+]);
